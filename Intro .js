18fed34e5f5ae7fbf3b60e9c5ccd5a9fdc966cab
chidimmaangellucy <chidimmaangellucy@gmail.com>
2026-09-12 13:24:36 +0100

    Intro .js assignment

---
+++ Intro .js
@@ -0,0 +1,3 @@
+console.log("my name is chinecherem");
+console.log("am 17 years old");
+console.log("my favorite programming language is JavaScript");
--- README.md
+++
@@ -1,2 +0,0 @@
-# JavaScript-assignment-
-My JavaScript introduction assignment 
